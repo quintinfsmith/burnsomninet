193a5ecee3b57e1d61a0b03af461283bb9b9a081
Quintin Smith <smith.quintin@protonmail.com>
2022-08-01 20:13:56 -0700

    allow trailing slashes on sections

--- burnsomninet/urls.py
+++ burnsomninet/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/<path:section_path>', views.api_controller),
     path("<str:section>/<str:subsection_path>", views.section_controller),
+    path("<str:section>/<str:subsection_path>/", views.section_controller),
     path("<str:section>/<str:subsection>.json", views.section_json),
 ]
 
