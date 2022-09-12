60b0ce30d0fee12050537c860bd2145effe90a96
Quintin Smith <smith.quintin@protonmail.com>
2022-09-12 10:22:14 -0700

    part of prev commit

---
+++ sitecode/py/accesslogmanager.py
@@ -0,0 +1,32 @@
+import os
+import mariadb
+import time
+from datetime import datetime, timezone
+
+def connect_to_mariadb():
+    return mariadb.connect(
+        user="http",
+        password="terpankerpanhorseradishblot",
+        host="localhost",
+ #       port=3306,
+        database="burnsomninet"
+
+    )
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def log_access(request):
+    connection = connect_to_mariadb()
+    cursor = connection.cursor()
+    query = f"INSERT INTO accesslog (`ip`, `path`) VALUE (?, ?);"
+    ip = get_client_ip(request)
+    cursor.execute(query, (ip, request.get_full_path()))
+    connection.commit()
+    connection.close()
+
