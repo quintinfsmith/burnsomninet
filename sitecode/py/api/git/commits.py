3c4161187576573da5372aa13132a72d5c359fa5
Quintin Smith <smith.quintin@protonmail.com>
2024-01-29 14:33:03 -0800

    Show all branches in overview when looking at master

--- sitecode/py/api/git/commits.py
+++ sitecode/py/api/git/commits.py
@@ -13,18 +13,21 @@ def process_request(**kwargs):
 
     fields = kwargs.get("fields", "date,description,id,author")
     fields = set(fields.split(","))
+    get_all_branches = kwargs.get("all_branches", False)
     # -------------------------------------------- #
+
     project = Project(f"/srv/git/{project_name}")
     all_project_branches = project.get_branch_names()
+
     if project_branch not in all_project_branches:
         project_branch = "master"
 
-    if project_branch == "*":
+    if get_all_branches:
         project_branches = all_project_branches
     else:
         project_branches = [project_branch]
 
-    output = []
+    mapped_commits = {}
     for branch_name in project_branches:
         branch  = project.get_branch(branch_name)
         commits = branch.get_commits()
@@ -34,19 +37,29 @@ def process_request(**kwargs):
             if timestamp < date_from or timestamp >= date_to:
                 continue
 
-            simple_commit = {}
-            if "date" in fields:
-                simple_commit['date'] = timestamp
-            if "description" in fields:
-                simple_commit['description'] = commit.get_description()
-            if "id" in fields:
-                simple_commit['id'] = commit.get_id()
+            commit_id = commit.get_id()
 
-            if "author" in fields:
-                simple_commit['author'] = commit.get_author_email()
+            if commit_id in mapped_commits.keys():
+                mapped_commits[commit_id]["branches"].append(branch_name)
+            else:
+                simple_commit = {
+                    "branches": [branch_name]
+                }
 
-            output.append(simple_commit)
+                if "date" in fields:
+                    simple_commit['date'] = timestamp
+                if "description" in fields:
+                    simple_commit['description'] = commit.get_description()
+                if "id" in fields:
+                    simple_commit['id'] = commit_id
+
+                if "author" in fields:
+                    simple_commit['author'] = commit.get_author_email()
+                mapped_commits[commit_id] = simple_commit
     # -------------------------------------------- #
+    output = []
+    for _i, commit in mapped_commits.items():
+        output.append(commit)
 
     return output
 
