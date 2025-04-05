ee8d00d12b0d3f215ddb1917e7fa093715e6e69b
Quintin Smith <smith.quintin@protonmail.com>
2025-04-04 20:52:37 -0700

    readded externals

--- sitecode/py/api/externals.py
+++ sitecode/py/api/externals.py
@@ -2,6 +2,7 @@ from sitecode.py.gitmanip import Project
 from sitecode.py.cachemanager import get_cached, check_cache, update_cache
 from sitecode.py import api
 from burnsomninet.views import STATIC_PATH
+
 import json
 from datetime import datetime, timedelta
 import time, os
@@ -15,18 +16,18 @@ def process_request(**kwargs):
     with open(f"{STATIC_PATH}/icons/email.svg", "r") as fp:
         svg_email = fp.read()
 
-    # svg_bsky = ""
-    # with open(f"{STATIC_PATH}/icons/bsky.svg", "r") as fp:
-    #     svg_bsky = fp.read()
+    #svg_bsky = ""
+    #with open(f"{STATIC_PATH}/icons/bsky.svg", "r") as fp:
+    #    svg_bsky = fp.read()
 
 
-    # svg_keybase = ""
-    # with open(f"{STATIC_PATH}/icons/keybase.svg", "r") as fp:
-    #     svg_keybase = fp.read()
+    svg_keybase = ""
+    with open(f"{STATIC_PATH}/icons/keybase.svg", "r") as fp:
+        svg_keybase = fp.read()
 
-    # svg_kofi = ""
-    # with open(f"{STATIC_PATH}/icons/kofi.svg", "r") as fp:
-    #     svg_kofi = fp.read()
+    svg_kofi = ""
+    with open(f"{STATIC_PATH}/icons/kofi.svg", "r") as fp:
+        svg_kofi = fp.read()
 
     return [
         {
@@ -38,20 +39,20 @@ def process_request(**kwargs):
             "href": "https://github.com/quintinfsmith",
             "alt": "Github",
             "svg": svg_github
+        },
+        #{
+        #    "href": "https://bsky.app/profile/quintinfsmith.bsky.social",
+        #    "alt": "Bluesky",
+        #    "svg": svg_bsky
+        #},
+        {
+            "href": "https://keybase.io/quintinfsmith",
+            "alt": "Keybase",
+            "svg": svg_keybase
+        },
+        {
+            "href": "https://ko-fi.com/quintinfsmith",
+            "alt": "Ko-Fi",
+            "svg": svg_kofi
         }
-        # {
-        #     "href": "https://bsky.app/profile/quintinfsmith.bsky.social",
-        #     "alt": "Bluesky",
-        #     "svg": svg_bsky
-        # },
-        # {
-        #     "href": "https://keybase.io/quintinfsmith",
-        #     "alt": "Keybase",
-        #     "svg": svg_keybase
-        # },
-        # {
-        #     "href": "https://ko-fi.com/quintinfsmith",
-        #     "alt": "Ko-Fi",
-        #     "svg": svg_kofi
-        # }
     ]
